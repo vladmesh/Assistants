8f26f7fdc660c1d493a1f877bc060b0ba0dc8be4
q <vladiclav.me@gmail.com>
2025-05-03 22:35:33 +0200

    fix database URL

--- rest_service/src/config.py
+++ rest_service/src/config.py
@@ -8,9 +8,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """Application settings"""
 
-    ASYNC_DATABASE_URL: str = os.getenv(
-        "ASYNC_DATABASE_URL", "postgresql+asyncpg://postgres:postgres@db:5432/postgres"
-    )
+    ASYNC_DATABASE_URL: str = os.getenv("ASYNC_DATABASE_URL")
     DB_ECHO: bool = False
     DB_POOL_SIZE: int = 5
     DB_MAX_OVERFLOW: int = 10
